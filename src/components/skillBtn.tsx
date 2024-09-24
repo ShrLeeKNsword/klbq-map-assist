9a4a61432a1eb86174f624d3baa0b095e12d42dc
爱走位的KN_sword <3478099037@qq.com>
2024-09-24 23:14:11 +0800

    added sidebar icon-buttons

---
+++ src/components/skillBtn.tsx
@@ -0,0 +1,22 @@
+interface SkillBtnPops {
+    imglink?: string;
+}
+
+const draggableBtnStyle = {
+    borderRadius: "100%",
+    margin: "3px",
+    width: "35px",
+    height: "35px",
+    display: 'flex',
+    placeItems: 'center',
+    placeContent: 'center',
+    backgroundColor: "rgba(var(--semi-grey-0), 1)",
+    boxShadow: "0 0 0 3px rgba(var(--semi-grey-1), 1)",
+    overflow: "hidden",
+  }
+
+const SkillBtn = (props: SkillBtnPops) => {
+    return <div style={draggableBtnStyle}><img src={props.imglink} style={{ width: '100%', height: '100%' }} /></div>
+}
+
+export default SkillBtn;
