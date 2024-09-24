7fb7fd947f786ca174f8d13feb72914f8a5eb954
爱走位的KN_sword <3478099037@qq.com>
2024-09-24 10:07:36 +0800

    Update App.tsx
    disable some language choice

--- src/App.tsx
+++ src/App.tsx
@@ -182,9 +182,9 @@ function App() {
           <div style={{ marginTop: "8px", marginRight: "12px" }}><IconLanguage size='extra-large' /></div>
           <Select defaultValue="简体中文" style={{ width: 120, marginTop: "18px" }} onChange={changePresentlanguage}>
             <Select.Option value="简体中文">简体中文</Select.Option>
-            <Select.Option value="繁体中文">繁体中文</Select.Option>
-            <Select.Option value="日本語">日本語</Select.Option>
-            <Select.Option value="English">English</Select.Option>
+            <Select.Option disabled value="繁体中文">繁体中文</Select.Option>
+            <Select.Option disabled value="日本語">日本語</Select.Option>
+            <Select.Option disabled value="English">English</Select.Option>
           </Select></div>
       </Header>
       <Layout>
