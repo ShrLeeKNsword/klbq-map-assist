e15ee2603fb1faff1c6ff9de006381b3e9361f25
Mieko Hikari <77004524+MiekoHikari@users.noreply.github.com>
2024-09-25 13:41:26 +0400

    turn palette into hex

--- src/utils/canvasConstants.ts
+++ src/utils/canvasConstants.ts
@@ -26,15 +26,15 @@ export enum mapTools {
 }
 
 export const colorPalette = [
-	"red",
-	"orange",
-	"yellow",
-	"green",
-	"blue",
-	"purple",
-	"white",
-	"rgba(var(--semi-grey-2), 1)",
-	"rgba(var(--semi-grey-4), 1)",
-	"rgba(var(--semi-grey-6), 1)",
-	"rgba(var(--semi-grey-8), 1)",
+	"#FF0000", // red
+	"#FFA500", // orange
+	"#FFFF00", // yellow
+	"#008000", // green
+	"#0000FF", // blue
+	"#800080", // purple
+	"#FFFFFF", // white
+	"#B0B0B0", // semi-grey-2
+	"#787878", // semi-grey-4
+	"#404040", // semi-grey-6
+	"#202020", // semi-grey-8
 ]
