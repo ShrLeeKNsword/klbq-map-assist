6fb8a2bc19e7a9579759e8f197d1a05f5a833d6a
爱走位的KN_sword <3478099037@qq.com>
2024-09-25 13:04:27 +0800

    added badge

--- src/components/grenadeBtn.tsx
+++ src/components/grenadeBtn.tsx
@@ -1,5 +1,8 @@
+import { Badge } from '@douyinfe/semi-ui';
+
 interface GrenadeBtnPops {
     imglink?: string;
+    badge?: string | null;
 }
 
 const draggableBtnStyle = {
@@ -13,10 +16,12 @@ const draggableBtnStyle = {
     backgroundColor: "rgba(var(--semi-grey-6), 1)",
     boxShadow: "0 0 0 3px rgba(var(--semi-grey-1), 1)",
     overflow: "hidden",
-  }
+}
 
 const GrenadeBtn = (props: GrenadeBtnPops) => {
-    return <div style={draggableBtnStyle}><img src={props.imglink} style={{ width: '100%', height: '100%' }} /></div>
+    return <>
+        {props.badge != null ? <Badge count={props.badge} position='rightBottom' type={props.badge==='×'?'danger':'secondary'}><div style={draggableBtnStyle}><img src={props.imglink} style={{ width: '100%', height: '100%' }} /></div></Badge> : <div style={draggableBtnStyle}><img src={props.imglink} style={{ width: '100%', height: '100%' }} /></div>}
+    </>
 }
 
 export default GrenadeBtn;
