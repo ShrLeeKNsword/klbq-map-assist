362a86f71cde6fd754a1406a8d1220da0d13da85
爱走位的KN_sword <3478099037@qq.com>
2024-09-27 10:54:31 +0800

    change Title words into svg

--- src/App.tsx
+++ src/App.tsx
@@ -148,7 +148,7 @@ function App() {
   return (
     <Layout className="components-layout-demo" style={{ height: 720, width: 1280, margin: "auto" }}>
       <Header style={styles.commonStyles}>
-        <Title heading={3} style={{ margin: '14px 0' }} >{presentLanguage.title} - {presentMap}</Title>
+        <Title heading={3} style={{ margin: '14px 0' }} >{presentLanguage.title} - {presentMap} <span>{mapMarkNameMode ? <MdCreate size='1.5rem' /> : <></>}</span></Title>
         <div style={{ position: "relative", left: "1100px", top: "-60px", height: "100%", width: "200px", display: "flex" }}>
           <div style={{ marginTop: "8px", marginRight: "12px" }}><MdOutlineTranslate size='1.5rem' /></div>
           <Select defaultValue={presentLanguage.language} style={{ width: 120, marginTop: "18px" }} onChange={value => changePresentlanguage(value as string)}>
@@ -331,7 +331,7 @@ function App() {
             <a href='https://github.com/ShrLeeKNsword/klbq-map-assist' target="_blank" style={{}}>
               <FaGithub />
             </a>
-            <a href='https://discord.com/invite/C6AYFvgR' target="_blank" style={{marginLeft:"5px"}}>
+            <a href='https://discord.com/invite/C6AYFvgR' target="_blank" style={{ marginLeft: "5px" }}>
               <FaDiscord />
             </a>
           </div>
