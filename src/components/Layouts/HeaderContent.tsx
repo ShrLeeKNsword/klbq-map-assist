b01f2ed3ce9365c7ebf2b349f116b7a14735a9f3
Mieko Hikari <77004524+MiekoHikari@users.noreply.github.com>
2024-09-28 17:34:12 +0400

    Layout refresh

---
+++ src/components/Layouts/HeaderContent.tsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import { i18nData, Languages } from '../../data/i18n'
+import { Button, Nav, Select, Tag } from '@douyinfe/semi-ui'
+import Title from '@douyinfe/semi-ui/lib/es/typography/title'
+import { MapName } from '../../data/maplist'
+import { MdOutlineTranslate } from 'react-icons/md'
+import { CgDarkMode } from 'react-icons/cg'
+
+// Extend the Window interface to include setMode
+declare global {
+  interface Window {
+    setMode: (mode: string) => void
+  }
+}
+
+// props
+interface HeaderContentProps {
+  currentLanguageMode: Languages
+  changeLanguage: React.Dispatch<React.SetStateAction<Languages>>
+  currentMap: MapName
+}
+
+const HeaderContent: React.FC<HeaderContentProps> = ({ changeLanguage, currentLanguageMode, currentMap }) => {
+  const currentLanguage = i18nData[currentLanguageMode]
+
+  console.log(i18nData[currentLanguageMode])
+
+  const themeMode = () => {
+    if (document.body.hasAttribute('theme-mode')) {
+      document.body.removeAttribute('theme-mode')
+      window.setMode('light')
+    } else {
+      document.body.setAttribute('theme-mode', 'dark')
+      window.setMode('dark')
+    }
+  }
+
+  return (
+    <div style={{ width: '100%' }}>
+      <Nav mode='horizontal'>
+        <Nav.Header>
+          <Title>{currentLanguage.title}</Title>
+        </Nav.Header>
+        <Nav.Item>
+          <Tag size='large' shape='circle' type='solid' color='red'>
+            {currentLanguage.mapsetting.maps[currentMap]}
+          </Tag>
+        </Nav.Item>
+        <Nav.Footer>
+          <Button
+            icon={<CgDarkMode size='1.5rem' style={{ padding: '0 1rem' }} color='rgba(var(--semi-grey-9), 1)' />}
+            onClick={themeMode}
+          />
+          <MdOutlineTranslate size='1.5rem' style={{ padding: '0 1rem' }} color='rgba(var(--semi-grey-9), 1)' />
+          <Select
+            defaultValue={Languages[currentLanguage.language]}
+            onChange={(value) => changeLanguage(value as Languages)}>
+            {Object.keys(i18nData).map((key) => (
+              <Select.Option key={key} value={key}>
+                {Languages[key as unknown as Languages]}
+              </Select.Option>
+            ))}
+          </Select>
+        </Nav.Footer>
+      </Nav>
+    </div>
+  )
+}
+
+export default HeaderContent
