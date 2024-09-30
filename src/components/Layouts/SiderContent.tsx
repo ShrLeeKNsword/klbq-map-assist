0ef0ce590e22f312c6fbd1a0a3ad13b76581fbc1
Mieko Hikari <77004524+MiekoHikari@users.noreply.github.com>
2024-09-30 12:56:59 +0400

    Remove popover
    In preparation for new sider

--- src/components/Layouts/SiderContent.tsx
+++ src/components/Layouts/SiderContent.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { I18nData } from '../../data/i18n'
-import { Avatar, Divider, TabPane, Tabs, Popover, Col, Row } from '@douyinfe/semi-ui'
+import { Avatar, Divider, TabPane, Tabs, Col, Row } from '@douyinfe/semi-ui'
 import { GiBroadsword, GiShield } from 'react-icons/gi'
-import { characterData, fetchCharLink, PUS, TheScissors, Urbino } from '../../data/characters'
+import { fetchCharLink, PUS, TheScissors, Urbino } from '../../data/characters'
 
 interface SiderContentProps {
   currentLanguage: I18nData
 }
 
 const SiderContent: React.FC<SiderContentProps> = ({ currentLanguage: currentLanguageMode }) => {
-  const onDragStart = (event: React.DragEvent<HTMLDivElement>, character: string, side: 'attack' | 'defense') => {
+  const onDragStart = (event: React.DragEvent<HTMLSpanElement>, character: string, side: 'attack' | 'defense') => {
     event.dataTransfer.setData('character', character)
     event.dataTransfer.setData('side', side)
   }
@@ -20,52 +20,31 @@ const SiderContent: React.FC<SiderContentProps> = ({ currentLanguage: currentLan
         <TabPane
           tab={
             <span>
-              <GiBroadsword style={{ marginRight: "5px", marginLeft: "5px" }} />
+              <GiBroadsword style={{ marginRight: '5px', marginLeft: '5px' }} />
               {currentLanguageMode.sidebar.attact}
             </span>
           }
           itemKey='1'>
-          <Row style={{ width: "100%" }}>
+          <Row style={{ width: '100%' }}>
             {Object.keys(TheScissors).map((key: unknown) => {
               return (
-                <Popover
-                  position='right'
-                  content={
-                    <div style={{ width: "max", margin: '0 auto', display: 'flex', padding: 10 }}>
-                      <Avatar src={characterData[key].skills.Active} style={{ padding: '0.5rem' }} />
-                      <Avatar src={characterData[key].skills.Passive} style={{ padding: '0.5rem' }} />
-                      <Avatar src={characterData[key].skills.Ultimate} style={{ padding: '0.5rem' }} />
-                    </div>
-                  }>
-                  <Col span={6} style={{ display: "flex", placeItems: "center", placeContent: "center" }}>
-                    <span draggable onDragStart={(e) => onDragStart(e, key as string, 'attack')}>
-                      <Avatar src={fetchCharLink(key, 'attack')} style={{ padding: '0.25rem' }} />
-                    </span>
-                  </Col>
-                </Popover>
-
+                <Col span={6} style={{ display: 'flex', placeItems: 'center', placeContent: 'center' }}>
+                  <span draggable onDragStart={(e) => onDragStart(e, key as string, 'attack')}>
+                    <Avatar src={fetchCharLink(key, 'attack')} style={{ padding: '0.25rem' }} />
+                  </span>
+                </Col>
               )
             })}
           </Row>
           <Divider dashed={true} margin={'1rem'} />
-          <Row style={{ width: "100%" }}>
+          <Row style={{ width: '100%' }}>
             {Object.keys(Urbino).map((key: unknown) => {
               return (
-                <Popover
-                  position='right'
-                  content={
-                    <div style={{ width: "max", margin: '0 auto', display: 'flex', padding: 10 }}>
-                      <Avatar src={characterData[key].skills.Active} style={{ padding: '0.5rem' }} />
-                      <Avatar src={characterData[key].skills.Passive} style={{ padding: '0.5rem' }} />
-                      <Avatar src={characterData[key].skills.Ultimate} style={{ padding: '0.5rem' }} />
-                    </div>
-                  }>
-                  <Col span={6} style={{ display: "flex", placeItems: "center", placeContent: "center" }}>
-                    <span draggable onDragStart={(e) => onDragStart(e, key as string, 'attack')}>
-                      <Avatar src={fetchCharLink(key, 'attack')} style={{ padding: '0.25rem' }} />
-                    </span>
-                  </Col>
-                </Popover>
+                <Col span={6} style={{ display: 'flex', placeItems: 'center', placeContent: 'center' }}>
+                  <span draggable onDragStart={(e) => onDragStart(e, key as string, 'attack')}>
+                    <Avatar src={fetchCharLink(key, 'attack')} style={{ padding: '0.25rem' }} />
+                  </span>
+                </Col>
               )
             })}
           </Row>
@@ -73,50 +52,31 @@ const SiderContent: React.FC<SiderContentProps> = ({ currentLanguage: currentLan
         <TabPane
           tab={
             <span>
-              <GiShield style={{ marginRight: "5px" }} />
+              <GiShield style={{ marginRight: '5px' }} />
               {currentLanguageMode.sidebar.defense}
             </span>
           }
           itemKey='2'>
-          <Row style={{ width: "100%" }}>
+          <Row style={{ width: '100%' }}>
             {Object.keys(PUS).map((key: unknown) => {
               return (
-                <Popover
-                  position='right'
-                  content={
-                    <div style={{ width: "max", margin: '0 auto', display: 'flex', padding: 10 }}>
-                      <Avatar src={characterData[key].skills.Active} style={{ padding: '0.5rem' }} />
-                      <Avatar src={characterData[key].skills.Passive} style={{ padding: '0.5rem' }} />
-                      <Avatar src={characterData[key].skills.Ultimate} style={{ padding: '0.5rem' }} />
-                    </div>
-                  }>
-                  <Col span={6} style={{ display: "flex", placeItems: "center", placeContent: "center" }}>
-                    <span draggable onDragStart={(e) => onDragStart(e, key as string, 'defense')}>
-                      <Avatar src={fetchCharLink(key, 'defense')} style={{ padding: '0.25rem' }} />
-                    </span>
-                  </Col>
-                </Popover>
+                <Col span={6} style={{ display: 'flex', placeItems: 'center', placeContent: 'center' }}>
+                  <span draggable onDragStart={(e) => onDragStart(e, key as string, 'defense')}>
+                    <Avatar src={fetchCharLink(key, 'defense')} style={{ padding: '0.25rem' }} />
+                  </span>
+                </Col>
               )
             })}
           </Row>
           <Divider dashed={true} margin={'1rem'} />
-          <Row style={{ width: "100%" }}>
+          <Row style={{ width: '100%' }}>
             {Object.keys(Urbino).map((key: unknown) => {
               return (
-                <Popover
-                  position='right'
-                  content={
-                    <div style={{ width: "max", margin: '0 auto', display: 'flex', padding: 10 }}>
-                      <Avatar src={characterData[key].skills.Active} style={{ padding: '0.5rem' }} />
-                      <Avatar src={characterData[key].skills.Passive} style={{ padding: '0.5rem' }} />
-                      <Avatar src={characterData[key].skills.Ultimate} style={{ padding: '0.5rem' }} />
-                    </div>
-                  }><Col span={6} style={{ display: "flex", placeItems: "center", placeContent: "center" }}>
-                    <span draggable onDragStart={(e) => onDragStart(e, key as string, 'defense')}>
-                      <Avatar src={fetchCharLink(key, 'defense')} style={{ padding: '0.25rem' }} />
-                    </span>
-                  </Col>
-                </Popover>
+                <Col span={6} style={{ display: 'flex', placeItems: 'center', placeContent: 'center' }}>
+                  <span draggable onDragStart={(e) => onDragStart(e, key as string, 'defense')}>
+                    <Avatar src={fetchCharLink(key, 'defense')} style={{ padding: '0.25rem' }} />
+                  </span>
+                </Col>
               )
             })}
           </Row>
