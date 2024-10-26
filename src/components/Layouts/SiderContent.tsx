77904bc15720d4e02cfcac9d00a1ffc7a8f07c69
爱走位的KN_sword <3478099037@qq.com>
2024-10-26 22:24:18 +0800

    test

--- src/components/Layouts/SiderContent.tsx
+++ src/components/Layouts/SiderContent.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { I18nData } from '../../data/i18n'
-import { Avatar, Divider, TabPane, Tabs, Col, Row, Tooltip, Card } from '@douyinfe/semi-ui'
+import { Avatar, Divider, TabPane, Tabs, Col, Row, Tooltip, Card, Popover, Descriptions } from '@douyinfe/semi-ui'
 import { GiBroadsword, GiShield, GiStunGrenade } from 'react-icons/gi'
 import { PUS, TheScissors, Urbino } from '../../data/characters/factions'
 import { characterData, characterRegistry } from '../../data/characters/characterRegistry'
 import { grenadeData } from '../../data/grenades'
+import { LuTimerReset } from "react-icons/lu";
 
 interface SiderContentProps {
   currentLanguage: I18nData
@@ -39,14 +40,75 @@ const SiderContent: React.FC<SiderContentProps> = ({ currentLanguage: currentLan
             {Object.keys(TheScissors).map((key: unknown) => {
               return (
                 <Col span={6} style={{ display: 'flex', placeItems: 'center', placeContent: 'center' }}>
-                  <Tooltip position='topLeft' content={key as string}>
+                  <Popover
+                    position='rightTop'
+                    content={<Card style={{ width: '500px', height: "100%", margin: "0 auto", overflow: "hidden" }}>
+                      <div style={{ display: "flex", height: "100%" }}>
+                        <div style={{ height: "100%", margin: "0 auto" }}><img draggable="false" src="https://s2.loli.net/2024/10/26/iD1g23pnC9d6hVM.png" style={{ height: "360px", filter: "drop-shadow(0 0 5px rgba(var(--semi-grey-7))" }}></img></div>
+                        <div style={{ width: "100%", marginTop: "5px", fontSize: "10px" }}>
+                          <Tabs type="button">
+                            <TabPane
+                              tab={
+                                <Avatar
+                                  src={characterRegistry!["Ming"].attack?.skills.active.skillIcon}
+                                  style={{ margin: '0 auto', height: "40px", width: "40px" }}
+                                />
+                              }
+                              itemKey="1"
+                            >
+                              <div style={{ margin: "3px" }}>
+                                明发射一个电球，其达到最大距离、碰到敌方超弦体时会自动引爆，也可通过手动再次按下技能键后主动引爆。电球会破坏范围内所有敌方单位（包含召唤物）的护甲并造成减速。明进入电场将提升移动速度。
+                              </div>
+                              <Descriptions
+                                style={{ marginTop: "10px" }}
+                                align='left'
+                                data={[
+                                  { key: "冷却", value: "25秒" },
+                                  { key: "最远距离", value: "60米" },
+                                  { key: "最短起爆", value: "8米" },
+                                  { key: "作用范围", value: "半径5米" },
+                                  { key: "移速提升", value: "20%" },
+                                ]}
+                              />
+                            </TabPane>
+                            <TabPane
+                              tab={
+                                <Avatar
+                                  src={characterRegistry!["Ming"].attack?.skills.passive.skillIcon}
+                                  style={{ margin: '0 auto', height: "40px", width: "40px" }}
+                                />
+                              }
+                              itemKey="2"
+                            >
+                              <div style={{ margin: "3px" }}>
+                                明使用枪械和主动技能对敌方护甲或护盾造成伤害时，自己的护甲将得到基于该伤害量的回复。绝招持续时间内，被动会回复临时护甲并增加回复量。
+                              </div>
+                            </TabPane>
+                            <TabPane
+                              tab={
+                                <Avatar
+                                  src={characterRegistry!["Ming"].attack?.skills.ultimate.skillIcon}
+                                  style={{ margin: '0 auto', height: "40px", width: "40px" }}
+                                />
+                              }
+                              itemKey="3"
+                            >
+                              <div style={{ margin: "3px" }}>
+                                明获得临时护甲并使自己的射击附带可叠加的减速效果。明使用枪械和主动技能对敌人的护甲造成伤害时，可延长该技能持续时间并回复临时护甲。
+                              </div>
+                            </TabPane>
+                          </Tabs>
+                        </div>
+                      </div>
+                    </Card>}
+                  >
                     <span draggable onDragStart={(e) => onDragStart(e, key as string, 'attack')}>
                       <Avatar
                         src={characterRegistry![key as string].attack?.canvasImage}
                         style={{ padding: '0.25rem' }}
                       />
                     </span>
-                  </Tooltip>
+                  </Popover>
                 </Col>
               )
             })}
@@ -137,48 +199,6 @@ const SiderContent: React.FC<SiderContentProps> = ({ currentLanguage: currentLan
         </TabPane>
       </Tabs>
       <Divider margin={'1rem'} />
-      <Card style={{ width: '100%', height: "100%", margin: "0 auto", overflow: "hidden" }}>
-        <div style={{ display: "flex" }}>
-          <div><img src="https://s2.loli.net/2024/10/26/iD1g23pnC9d6hVM.png" style={{ height: "200px" }}></img></div>
-          <div style={{ width: "100%" ,marginTop:"5px",fontSize:"10px"}}>
-            <Tabs type="button">
-              <TabPane
-                tab={
-                  <Avatar
-                    src={characterRegistry!["Ming"].attack?.skills.active.skillIcon}
-                    style={{ margin: '0 auto', height: "22px", width: "22px" }}
-                  />
-                }
-                itemKey="1"
-              >
-                明发射一个电球，其达到最大距离、碰到敌方超弦体时会自动引爆，也可通过手动再次按下技能键后主动引爆。电球会破坏范围内所有敌方单位（包含召唤物）的护甲并造成减速。明进入电场将提升移动速度。
-              </TabPane>
-              <TabPane
-                tab={
-                  <Avatar
-                    src={characterRegistry!["Ming"].attack?.skills.passive.skillIcon}
-                    style={{ margin: '0 auto', height: "22px", width: "22px" }}
-                  />
-                }
-                itemKey="2"
-              >
-                明使用枪械和主动技能对敌方护甲或护盾造成伤害时，自己的护甲将得到基于该伤害量的回复。绝招持续时间内，被动会回复临时护甲并增加回复量。
-              </TabPane>
-              <TabPane
-                tab={
-                  <Avatar
-                    src={characterRegistry!["Ming"].attack?.skills.ultimate.skillIcon}
-                    style={{ margin: '0 auto', height: "22px", width: "22px" }}
-                  />
-                }
-                itemKey="3"
-              >
-                明获得临时护甲并使自己的射击附带可叠加的减速效果。明使用枪械和主动技能对敌人的护甲造成伤害时，可延长该技能持续时间并回复临时护甲。
-              </TabPane>
-            </Tabs>
-          </div>
-        </div>
-      </Card>
     </div>
   )
 }
