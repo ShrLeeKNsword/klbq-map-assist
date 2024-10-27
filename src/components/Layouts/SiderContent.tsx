337a3aef4f457e873ca07c8d649e3c6f8a48fb96
爱走位的KN_sword <3478099037@qq.com>
2024-10-27 13:08:48 +0800

    Update SiderContent.tsx

--- src/components/Layouts/SiderContent.tsx
+++ src/components/Layouts/SiderContent.tsx
@@ -44,8 +44,14 @@ const SiderContent: React.FC<SiderContentProps> = ({ currentLanguage: currentLan
                     position='rightTop'
                     content={<Card style={{ width: '500px', height: "100%", margin: "0 auto", overflow: "hidden" }}>
                       <div style={{ display: "flex", height: "100%" }}>
-                        <div style={{ height: "100%", margin: "0 auto" }}><img draggable="false" src="https://s2.loli.net/2024/10/26/iD1g23pnC9d6hVM.png" style={{ height: "360px", filter: "drop-shadow(0 0 5px rgba(var(--semi-grey-7))" }}></img></div>
-                        <div style={{ width: "100%", marginTop: "5px", fontSize: "10px" }}>
+                        <div style={{ height: "360px", width: "200px", margin: "0 auto" }}>
+                          <img draggable="false" src="https://s2.loli.net/2024/10/26/iD1g23pnC9d6hVM.png" style={{ height: "360px", filter: "drop-shadow(0 0 5px rgba(var(--semi-grey-7))" }} />
+                          <div style={{ position: "absolute", left: "40px", bottom: "60px", height: "25px", display: "flex", alignContent: "center", filter: "drop-shadow(0 0 5px rgba(var(--semi-grey-7))" }}>
+                            <img draggable="false" src="https://s2.loli.net/2024/10/27/c7QDINMXFyuav6b.png" style={{ filter: "invert(100%)", height: "25px" }} />
+                            <span style={{ fontSize: "18px", margin: "1.5px", marginLeft: "5px", color: "white" }}><strong>决斗</strong></span>
+                          </div>
+                        </div>
+                        <div style={{ width: "100%", fontSize: "10px", marginRight: "15px", marginTop: "3px" }}>
                           <Tabs type="button">
                             <TabPane
                               tab={
@@ -56,17 +62,21 @@ const SiderContent: React.FC<SiderContentProps> = ({ currentLanguage: currentLan
                               }
                               itemKey="1"
                             >
-                              <div style={{ margin: "3px" }}>
+                              <div className='none-scrollbar' style={{ margin: "2px", height: "70px", overflowY: "scroll" }}>
                                 明发射一个电球，其达到最大距离、碰到敌方超弦体时会自动引爆，也可通过手动再次按下技能键后主动引爆。电球会破坏范围内所有敌方单位（包含召唤物）的护甲并造成减速。明进入电场将提升移动速度。
                               </div>
                               <Descriptions
-                                style={{ marginTop: "10px" }}
+                                className='none-scrollbar'
+                                style={{ marginTop: "10px", maxHeight: "190px", overflowY: "scroll" }}
                                 align='left'
                                 data={[
                                   { key: "冷却", value: "25秒" },
                                   { key: "最远距离", value: "60米" },
                                   { key: "最短起爆", value: "8米" },
                                   { key: "作用范围", value: "半径5米" },
+                                  { key: "移速提升", value: "20%" },
+                                  { key: "移速提升", value: "20%" },
+                                  { key: "移速提升", value: "20%" },
                                   { key: "移速提升", value: "20%" },
                                 ]}
                               />
